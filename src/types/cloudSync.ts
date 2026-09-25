0e44a7adb0f0a809b88503530dd856f11e323c2a
Alexandre De Carli <alexandre@potencial.tec.br>
2026-09-25 15:09:47 -0300

    feat(sync): define ProjectCloudSyncConfig and ProjectSyncBundle types

---
+++ src/types/cloudSync.ts
@@ -0,0 +1,42 @@
+import { Project, Mission } from './index';
+
+export interface ProjectCloudSyncConfig {
+    syncId: string;
+    syncKeyEncrypted?: string;
+    lastSyncedAt?: string;
+    workerUrl?: string;
+}
+
+export interface ProjectSyncBundle {
+    version: 1;
+    projectId: string;
+    syncId: string;
+    exportedAt: string;
+    project: Project;
+    missions: Mission[];
+}
+
+export interface CloudSyncPushOptions {
+    workerUrl: string;
+    syncId: string;
+    passkey: string;
+    project: Project;
+    missions: Mission[];
+}
+
+export interface CloudSyncPullOptions {
+    workerUrl: string;
+    syncId: string;
+    passkey: string;
+}
+
+export interface CloudSyncStatusResult {
+    exists: boolean;
+    lastModified?: string;
+    etag?: string;
+}
+
+export interface CloudSyncPushResult {
+    ok: boolean;
+    syncedAt: string;
+}
